2ae28cef40a3deae5f11e99b26f1d69af0090949
nike1v <dgygurdenok@gmail.com>
2021-07-06 14:59:36 +0300

    code pretty more

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 const sqlite3 = require("sqlite3").verbose();
-const cheerio = require('cheerio');
+const cheerio = require("cheerio");
 
-const db = new sqlite3.Database(':memory:');
+const db = new sqlite3.Database(":memory:");
 const urlParse = "https://www.rottentomatoes.com/browse/opening";
 
 const fetching = async () => {
@@ -12,27 +12,39 @@ const fetching = async () => {
   const $ = cheerio.load(req.data);
   $(".media-list__movie-info").each((i, el) => {
     listItems.push({
-      title: $(el).find('.media-list__title').text(),
-      actors: $(el).find(".media-list__actors").each((i, el) => el).text(),
-      consensus: $(el).find(".media-list__consensus").each((i, el) => el).text(),
+      title: $(el).find(".media-list__title").text(),
+      actors: $(el)
+        .find(".media-list__actors")
+        .each((i, el) => el)
+        .text(),
+      consensus: $(el)
+        .find(".media-list__consensus")
+        .each((i, el) => el)
+        .text(),
       date: $(el).find(".media-list__release-date").text(),
-    })
+    });
   });
 
   db.serialize(() => {
-    db.run("CREATE TABLE films(title varchar(255), actors varchar(255), consensus varchar(255), date varchar(255))");
+    db.run(
+      "CREATE TABLE films(title varchar(255), actors varchar(255), consensus varchar(255), date varchar(255))"
+    );
     listItems.map((film) => {
-    db.run("INSERT INTO films (title, actors, consensus, date) VALUES (?, ?, ?, ?)", [film.title, film.actors, film.consensus, film.date], (err) => {
-      if (err) {
-        console.log(err);
-      }
-    })
+      db.run(
+        "INSERT INTO films (title, actors, consensus, date) VALUES (?, ?, ?, ?)",
+        [film.title, film.actors, film.consensus, film.date],
+        (err) => {
+          if (err) {
+            console.log(err);
+          }
+        }
+      );
+    });
+
+    db.each("SELECT * from films", (err, row) => console.log(row));
   });
 
-    db.each("SELECT * from films", (err, row) => console.log(row))
-  })
-
   db.close();
-}
+};
 
 fetching();
